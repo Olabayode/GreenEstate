72875bded936dfc4b34e6fb6f8c9c85a2ba2214a
Emmanuel Olabayode <emmanuelolabayode@gmail.com>
2022-11-28 11:53:47 +0000

    more

--- greenestate/settings.py
+++ greenestate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-d4be.up.railway.app', '127.0.0:1']
 
 
 # Application definition
